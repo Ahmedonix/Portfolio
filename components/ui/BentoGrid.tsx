23bd425c0e82c6d14d0762edc5404faf25dbace1
Ahmedonix <ahmednq.dev@gmail.com>
2025-02-09 20:36:00 +0500

    deploying

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,18 +1,15 @@
-"use client"
-
-import { useState } from "react";
-import { IoCopyOutline } from "react-icons/io5";
-
-// Also install this npm i --save-dev @types/react-lottie
-import Lottie from "react-lottie";
-
-import { cn } from "@/lib/utils";
-
-
-import { BackgroundGradientAnimation } from "./GradientBg";
-import GridGlobe from "./GridGlobe";
+'use client';
+import { cn } from '@/lib/utils';
+import { BackgroundGradientAnimation } from './GradientBg';
+import GridGlobe from './GridGlobe';
+// import Lottie from 'react-lottie';
+import { useState } from 'react';
 import animationData from "@/data/confetti.json";
-import MagicButton from "./MagicButton";
+import { IoCopyOutline } from 'react-icons/io5';
+import MagicButton from './MagicButton';
+import dynamic from 'next/dynamic';
+
+const Lottie = dynamic(() => import('react-lottie'), { ssr: false });
 
 export const BentoGrid = ({
   className,
